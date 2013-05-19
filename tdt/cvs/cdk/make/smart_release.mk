cb9146348017ccc410562350dc328a1d6ac4f997
Teslanet <teslanet@teslanet-virtual-machine.(none)>
2013-05-19 15:55:47 +0300

    fix smart_release.mk

--- tdt/cvs/cdk/make/smart_release.mk
+++ tdt/cvs/cdk/make/smart_release.mk
@@ -1,6 +1,32 @@
 #
 # INIT-SCRIPTS customized
 #
+BEGIN[[
+init_scripts
+  0.10
+  {PN}-{PV}
+  pdircreate:{PN}-{PV}
+  nothing:file://../root/etc/inittab
+  nothing:file://../root/release/hostname
+  nothing:file://../root/release/inetd
+# for 'nothing:' only 'cp' is executed so '*' is ok.
+  nothing:file://../root/release/initmodules_*
+  nothing:file://../root/release/halt_*
+  nothing:file://../root/release/mountall
+  nothing:file://../root/release/mountsysfs
+  nothing:file://../root/release/networking
+  nothing:file://../root/release/rc
+  nothing:file://../root/release/reboot
+  nothing:file://../root/release/sendsigs
+  nothing:file://../root/release/telnetd
+  nothing:file://../root/release/syslogd
+  nothing:file://../root/release/crond
+  nothing:file://../root/release/umountfs
+  nothing:file://../root/release/lircd
+  nothing:file://../root/etc/init.d/avahi-daemon
+  nothing:file://../root/etc/init.d/rdate
+;
+]]END
 
 DESCRIPTION_init_scripts = init scripts and rules for system start
 init_scripts_initd_files = \
@@ -17,7 +43,8 @@ telnetd \
 syslogd \
 crond \
 lircd \
-umountfs
+umountfs \
+avahi-daemon
 
 define postinst_init_scripts
 #!/bin/sh
@@ -88,8 +115,23 @@ $(DEPDIR)/fonts-extra: $(addsuffix .ttf, $(addprefix root/usr/share/fonts/,$(fon
 #
 # 3G MODEMS
 #
+BEGIN[[
+modem_scripts
+  0.4
+  {PN}-{PV}
+  pdircreate:{PN}-{PV}
+  nothing:file://../root/etc/ppp/ip-*
+  nothing:file://../root/usr/bin/modem.sh
+  nothing:file://../root/usr/bin/modemctrl.sh
+  nothing:file://../root/etc/modem.conf
+  nothing:file://../root/etc/modem.list
+  nothing:file://../root/etc/55-modem.rules
+  nothing:file://../root/etc/30-modemswitcher.rules
+;
+]]END
+
 DESCRIPTION_modem_scripts = utils to setup 3G modems
-RDEPENDS_modem_scripts = pppd usb_modeswitch
+RDEPENDS_modem_scripts = pppd usb_modeswitch iptables iptables-dev
 
 $(DEPDIR)/modem-scripts: $(DEPENDS_modem_scripts) $(RDEPENDS_modem_scripts)
 	$(PREPARE_modem_scripts)
@@ -124,9 +166,28 @@ $(DEPDIR)/driver-ptinp:
 	$(toflash_build)
 	touch $@
 
+DESCRIPTION_driver_encrypt = driver encrypt
+PKGV_driver_encrypt = 0.1
+$(eval export PKGV_driver_encrypt = $(PKGV_driver_encrypt)$(KERNELSTMLABEL))
+SRC_URI_driver_encrypt = unknown
+
+$(DEPDIR)/driver-encrypt:
+	$(start_build)
+	mkdir -p $(PKDIR)/lib/modules/$(KERNELVERSION)/extra/encrypt
+	$(toflash_build)
+	touch $@
 #
 # UDEV RULES
 #
+BEGIN[[
+udev_rules
+  0.2
+  {PN}-{PV}
+  pdircreate:{PN}-{PV}
+  nothing:file://../root/etc/90-cec_proton.rules
+;
+]]END
+
 DESCRIPTION_udev_rules = custom udev rules
 RDEPENDS_udev_rules = udev
 
@@ -135,7 +196,62 @@ $(DEPDIR)/udev-rules: $(DEPENDS_udev_rules) $(RDEPENDS_udev_rules)
 	$(start_build)
 	cd $(DIR_udev_rules) && \
 	$(INSTALL_DIR) $(PKDIR)/etc/udev/rules.d/ && \
-	$(INSTALL_FILE) 90-cec_proton.rules $(PKDIR)/etc/udev/rules.d/
+	$(INSTALL_FILE) 90-cec_proton.rules $(PKDIR)/etc/udev/rules.d/ && \
+	$(toflash_build)
+	touch $@
+#
+# boot-elf
+#
+
+DESCRIPTION_boot_elf = firmware non public
+SRC_URI_boot_elf = unknown
+PKGV_boot_elf = r2
+RDEPENDS_boot_elf :=
+
+
+$(DEPDIR)/boot-elf: firmware $(RDEPENDS_boot_elf)
+	$(start_build)
+	$(INSTALL_DIR) $(PKDIR)/lib/firmware/
+	$(INSTALL_FILE) $(archivedir)/boot/video_7109.elf $(PKDIR)/lib/firmware/video.elf
+	$(INSTALL_FILE) $(archivedir)/boot/audio_7109.elf $(PKDIR)/lib/firmware/audio.elf
+	$(toflash_build)
+	touch $@
+
+#
+# opkg plugin nonfree feed
+#
+
+DESCRIPTION_enigma2_plugin_nonfree_feed = Download non free plugins and skins
+SRC_URI_enigma2_plugin_nonfree_feed = unknown
+PKGV_enigma2_plugin_nonfree_feed = r3
+RDEPENDS_enigma2_plugin_nonfree_feed :=
+
+define postinst_enigma2_plugin_nonfree_feed
+#!/bin/sh
+# postinst script for nonfree-feed
+(sleep 10; opkg update) &
+exit 0
+endef
+
+$(DEPDIR)/enigma2-plugin-nonfree-feed: $(RDEPENDS_enigma2_plugin_nonfree_feed)
+	$(start_build)
+	$(INSTALL_DIR) $(PKDIR)/etc/opkg/ && \
+	$(INSTALL_FILE) $(buildprefix)/root/etc/ipkg/non-free-feed.conf $(PKDIR)/etc/opkg/non-free-feed.conf
+	$(e2extra_build)
+	touch $@
+	
+#
+# firmware
+#
+
+DESCRIPTION_firmware = firmware non public
+SRC_URI_firmware = unknown
+PKGV_firmware = r1
+RDEPENDS_firmware :=
+
+$(DEPDIR)/firmware:  $(RDEPENDS_firmware)
+	$(start_build)
+	$(INSTALL_DIR) $(PKDIR)/lib/firmware/
 	$(toflash_build)
 	touch $@
 
@@ -147,19 +263,6 @@ release_common_utils:
 	cp -f $(buildprefix)/root/release/official-feed.conf $(prefix)/release/etc/opkg/
 	cp -f $(buildprefix)/root/release/opkg.conf $(prefix)/release/etc/
 	$(call initdconfig,$(shell ls $(prefix)/release/etc/init.d))
-
-# Copy video_7109
-	cp -f $(archivedir)/boot/video_7109.elf $(prefix)/release/boot/video.elf
-# Copy audio_7109
-	cp -f $(archivedir)/boot/audio_7109.elf $(prefix)/release/boot/audio.elf
-
-	find $(prefix)/release/lib/modules/ -name '*.so' -exec rm -f {} \;
-	cp -dp $(buildprefix)/root/release/auto_rb_st.sh $(prefix)/release/etc/init.d
-	cp -dp $(buildprefix)/root/etc/init.d/rdate.sh $(prefix)/release/etc/init.d
-	ln -sf /etc/init.d/rdate.sh $(prefix)/release/etc/rc.d/rcS.d/S99rdate.sh
-	mkdir -p $(prefix)/release/var/bin
-	rm -rf $(prefix)/release/usr/lib/enigma2/python/Plugins/enigma2_plugin_extensions_simpleumount-0.09-py$(PYTHON_VERSION).egg-info
-	cp -dp $(buildprefix)/root/etc/11-usbhd-automount.rules $(prefix)/release/etc/udev/rules.d
 	
 release_base: driver-ptinp
 	rm -rf $(prefix)/release || true
@@ -185,7 +288,12 @@ release_base: driver-ptinp
 	$(INSTALL_DIR) $(prefix)/release/etc/tuxbox && \
 	$(INSTALL_DIR) $(prefix)/release/etc/enigma2 && \
 	$(INSTALL_DIR) $(prefix)/release/media && \
+	$(INSTALL_DIR) $(prefix)/release/media/dvd && \
 	$(INSTALL_DIR) $(prefix)/release/media/net && \
+	$(INSTALL_DIR) $(prefix)/release/mnt && \
+	$(INSTALL_DIR) $(prefix)/release/mnt/usb && \
+	$(INSTALL_DIR) $(prefix)/release/mnt/hdd && \
+	$(INSTALL_DIR) $(prefix)/release/mnt/nfs && \
 	$(INSTALL_DIR) $(prefix)/release/root && \
 	$(INSTALL_DIR) $(prefix)/release/proc && \
 	$(INSTALL_DIR) $(prefix)/release/sys && \
@@ -193,6 +301,8 @@ release_base: driver-ptinp
 	$(INSTALL_DIR) $(prefix)/release/usr && \
 	$(INSTALL_DIR) $(prefix)/release/usr/bin && \
 	$(INSTALL_DIR) $(prefix)/release/media/hdd && \
+	$(INSTALL_DIR) $(prefix)/release/media/hdd/music && \
+	$(INSTALL_DIR) $(prefix)/release/media/hdd/picture && \
 	$(INSTALL_DIR) $(prefix)/release/lib && \
 	$(INSTALL_DIR) $(prefix)/release/lib/modules && \
 	$(INSTALL_DIR) $(prefix)/release/lib/firmware && \
@@ -207,6 +317,7 @@ release_base: driver-ptinp
 # rc.d directories
 	mkdir -p $(prefix)/release/etc/rc.d/rc{0,1,2,3,4,5,6,S}.d
 	ln -sf ../init.d $(prefix)/release/etc/rc.d
+
 # zoneinfo
 	$(INSTALL_DIR) $(prefix)/release/usr/share/zoneinfo && \
 	cp -aR $(buildprefix)/root/usr/share/zoneinfo/* $(prefix)/release/usr/share/zoneinfo/
@@ -241,16 +352,11 @@ release_base: driver-ptinp
 	fi
 
 # Copy lircd.conf
-	cp -f $(buildprefix)/root/etc/lircd_hl101.conf $(prefix)/release/etc/lircd.conf
-
 	touch $(prefix)/release/var/etc/.firstboot && \
-	cp -f $(buildprefix)/root/release/mme_check $(prefix)/release/etc/init.d/ && \
 	cp -f $(buildprefix)/root/bootscreen/bootlogo.mvi $(prefix)/release/boot/ && \
 	cp -f $(buildprefix)/root/bin/autologin $(prefix)/release/bin/ && \
 	cp -f $(buildprefix)/root/bin/vdstandby $(prefix)/release/bin/ && \
 	cp -f $(buildprefix)/root/etc/vdstandby.cfg $(prefix)/release/etc/ && \
-	cp -f $(buildprefix)/root/etc/init.d/avahi-daemon $(prefix)/release/etc/init.d/ && \
-	chmod 777 $(prefix)/release/etc/init.d/avahi-daemon && \
 	cp -f $(buildprefix)/root/etc/network/interfaces $(prefix)/release/etc/network/ && \
 	cp -f $(buildprefix)/root/sbin/flash_* $(prefix)/release/sbin/ && \
 	cp -f $(buildprefix)/root/sbin/nand* $(prefix)/release/sbin/ && \
@@ -263,6 +369,7 @@ release_base: driver-ptinp
 	cp -dp $(buildprefix)/root/etc/inetd.conf $(prefix)/release/etc/ && \
 	ln -s /usr/share/zoneinfo/Europe/Berlin $(prefix)/release/etc/localtime && \
 	cp -dp $(targetprefix)/etc/mtab $(prefix)/release/etc/ && \
+	cp -dp $(buildprefix)/root/etc/motd $(prefix)/release/etc/ && \
 	cp -dp $(targetprefix)/etc/passwd $(prefix)/release/etc/ && \
 	cp -dp $(buildprefix)/root/etc/profile $(prefix)/release/etc/ && \
 	cp -dp $(targetprefix)/etc/protocols $(prefix)/release/etc/ && \
@@ -277,31 +384,20 @@ release_base: driver-ptinp
 	$(INSTALL_FILE) root/etc/tuxbox/timezone.xml $(prefix)/release/etc/ && \
 	echo "576i50" > $(prefix)/release/etc/videomode
 
-release_cube_common:
-	cp $(buildprefix)/root/release/reboot_cuberevo $(prefix)/release/etc/init.d/reboot && \
-	chmod 777 $(prefix)/release/etc/init.d/reboot && \
-	cp $(buildprefix)/root/bin/eeprom $(prefix)/release/bin
-
-#release_spark:
-#	echo "spark" > $(prefix)/release/etc/hostname && \
-#	echo "src/gz AR-P http://alien.sat-universum.de" | cat - $(prefix)/release/etc/opkg/official-feed.conf > $(prefix)/release/etc/opkg/official-feed && \
-#	mv $(prefix)/release/etc/opkg/official-feed $(prefix)/release/etc/opkg/official-feed.conf && \
-#	echo "src/gz plugins-feed http://extra.sat-universum.de" > $(prefix)/release/etc/opkg/plugins-feed.conf && \
-#	cp $(buildprefix)/root/etc/lircd_spark.conf.09_00_0B $(prefix)/release/etc/lircd.conf.09_00_0B && \
-#	cp $(buildprefix)/root/firmware/component_7111_mb618.fw $(prefix)/release/lib/firmware/component.fw && \
-#	true
-
 release_hl101:
 	echo "hl101" > $(prefix)/release/etc/hostname && \
 	cp -f $(buildprefix)/root/release/fstab_hl101 $(prefix)/release/etc/fstab
 	cp $(buildprefix)/root/firmware/dvb-fe-avl2108.fw $(prefix)/release/lib/firmware/ && \
 	cp $(buildprefix)/root/firmware/dvb-fe-stv6306.fw $(prefix)/release/lib/firmware/
+	cp -dp $(buildprefix)/root/release/auto_rb_st.sh $(prefix)/release/etc/init.d
+	cp -dp $(buildprefix)/root/etc/init.d/rdate.sh $(prefix)/release/etc/init.d
+	ln -sf /etc/init.d/rdate.sh $(prefix)/release/etc/rc.d/rcS.d/S99rdate.sh
+
 
 # The main target depends on the model.
 # IMPORTANT: it is assumed that only one variable is set. Otherwise the target name won't be resolved.
 #
-$(DEPDIR)/min-release $(DEPDIR)/std-release $(DEPDIR)/max-release $(DEPDIR)/release: \
-$(DEPDIR)/%release:release_base release_common_utils release_hl101
+$(DEPDIR)/release: $(DEPDIR)/%release:boot-elf release_base release_common_utils release_hl101
 # Post tweaks
 	$(DEPMOD) -b $(prefix)/release $(KERNELVERSION)
 	touch $@
@@ -309,9 +405,8 @@ $(DEPDIR)/%release:release_base release_common_utils release_hl101
 release-clean:
 	rm -f $(DEPDIR)/release
 	rm -f $(DEPDIR)/release_base
-	rm -f $(DEPDIR)/release_hl101
+	rm -f $(DEPDIR)/release_$(HL101)
 	rm -f $(DEPDIR)/release_common_utils 
-	rm -f $(DEPDIR)/release_cube_common
 
 ######## FOR YOUR OWN CHANGES use these folder in cdk/own_build/enigma2 #############
 	cp -RP $(buildprefix)/own_build/enigma2/* $(prefix)/release/
