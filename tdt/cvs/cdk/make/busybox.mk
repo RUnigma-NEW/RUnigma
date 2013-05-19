ab90ee01880f2a739408aa4f47071ef1ac7ff15f
Teslanet <teslanet@teslanet-virtual-machine.(none)>
2013-05-19 12:43:29 +0300

    fix busybox.mk

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -1,34 +1,39 @@
 #
 # busybox
 #
-$(DEPDIR)/busybox.do_prepare: $(DEPENDS_busybox)
-	$(PREPARE_busybox)
-	touch $@
 
-$(DEPDIR)/busybox.do_compile: bootstrap $(DEPDIR)/busybox.do_prepare Patches/busybox.config | $(DEPDIR)/$(GLIBC_DEV)
-	cd $(DIR_busybox) && \
-		export CROSS_COMPILE=$(target)- && \
-		$(MAKE) mrproper && \
-		$(INSTALL) -m644 ../$(lastword $^) .config && \
-		$(MAKE) all \
-			CROSS_COMPILE=$(target)- \
-			CFLAGS_EXTRA="$(TARGET_CFLAGS)"
-	touch $@
+PKGR_busybox = r2
+BEGIN[[
+busybox
+  1.21.0
+  {PN}-{PV}
+  extract:http://www.{PN}.net/downloads/{PN}-{PV}.tar.bz2
+  patch:file://{PN}-{PV}-mdev.patch
+  patch:file://{PN}-{PV}-platform.patch
+  patch:file://{PN}-{PV}-xz.patch
+  patch:file://{PN}-{PV}-ntfs.patch
+  nothing:file://{PN}-{PV}.config
+  pmove:{PN}-{PV}/{PN}-{PV}.config:{PN}-{PV}/.config
+  make:install:CONFIG_PREFIX=PKDIR
+;
+]]END
 
 DESCRIPTION_busybox = "Utilities for embedded systems"
 
-$(DEPDIR)/min-busybox $(DEPDIR)/std-busybox $(DEPDIR)/max-busybox \
-$(DEPDIR)/busybox: \
-$(DEPDIR)/%busybox: $(DEPDIR)/busybox.do_compile
+$(DEPDIR)/busybox: bootstrap $(DEPENDS_busybox) $(buildprefix)/Patches/busybox.config
+	$(PREPARE_busybox)
 	$(start_build)
 	cd $(DIR_busybox) && \
 		export CROSS_COMPILE=$(target)- && \
+		$(MAKE) all \
+			CROSS_COMPILE=$(target)- \
+			CONFIG_EXTRA_CFLAGS="$(TARGET_CFLAGS)" && \
 		$(INSTALL_busybox)
-#		@CLEANUP_busybox@
 	install -m644 -D /dev/null $(PKDIR)/etc/shells
 	export HHL_CROSS_TARGET_DIR=$(PKDIR) && $(hostprefix)/bin/target-shellconfig --add /bin/ash 5
 	$(tocdk_build)
 	$(toflash_build)
-	[ "x$*" = "x" ] && touch $@ || true
+#	$(CLEANUP_busybox)
+	touch $@
 
 $(eval $(call guiconfig,busybox))
