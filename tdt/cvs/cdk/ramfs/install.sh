abf0da15487326117151e75a2b6127bdc0e84bf7
ACC-NEW <piterkadet@inbox.ru>
2013-06-12 09:58:52 +0400

    Update install.sh

--- tdt/cvs/cdk/ramfs/install.sh
+++ tdt/cvs/cdk/ramfs/install.sh
@@ -1,24 +1,24 @@
-echo "Copy Image to Ram"
-echo "Copy Image" > /dev/vfd
+echo "Копирую образ в Оперативную память"
+echo "Копирую" > /dev/vfd
 cd /rootfs
 cp *.tar.gz /install
-echo "done"
-echo "Save Settings" > /dev/vfd
+echo "Готово"
+echo "Сохраняю" > /dev/vfd
 if [ -e /rootfs/etc/enigma2 ]; then
-	echo "Sichere Settings"
-	echo "Save Settings" > /dev/vfd
+	echo "Проверяю, есть ли пользовательские настройки Энигмы..."
+	echo "Сохраняю" > /dev/vfd
 	cd /rootfs/etc/enigma2
 	rm settings
 	tar -czvf /install/backup/E2Settings.tar.gz ./ > /dev/null 2>&1
 	cd /
 else
-	echo "keine Settings gefunden"
-	echo "checke SDA2"
+	echo "Настройки не найдены"
+	echo "Проверяю раздел SDA2"
 	cd /
 	umount /dev/sda1
 	mount /dev/sda2 /rootfs
 	if [ -e /rootfs/etc/enigma2 ]; then
-		echo "Sichere Settings"
+		echo "Проверяю, есть ли пользовательские настройки Энигмы..."
 		cd /rootfs/etc/enigma2
 		rm settings
 		tar -czvf /install/backup/E2Settings.tar.gz ./ > /dev/null 2>&1
@@ -26,7 +26,7 @@ else
 		umount /dev/sda2
 		mount /dev/sda1 /rootfs
 	else
-		echo "Keine Settings auf SDA2 gefunden"
+		echo "Настройки в разделе SDA2 не найдены"
 		cd /
 		umount /dev/sda2
 		mount /dev/sda1 /rootfs
@@ -35,37 +35,37 @@ fi
 if [ -e /rootfs/var/keys ]; then
 	cd /rootfs/var/keys
 	tar -czvf /install/backup/keys.tar.gz ./ > /dev/null 2>&1
-	echo "done"
+	echo "Готово"
 	cd /
 else
-	echo "keine keys gefunden"
-	echo "checke SDA2"
+	echo "Ключи не найдены"
+	echo "Проверяю раздел  SDA2"
 	cd /
 	umount /dev/sda1
 	mount /dev/sda2 /rootfs
 	if [ -e /rootfs/var/keys ]; then
 		cd /rootfs/var/keys
 		tar -czvf /install/backup/keys.tar.gz ./ > /dev/null 2>&1
-		echo "done"
+		echo "Готово"
 		cd /
 		umount /dev/sda2
 		mount /dev/sda1 /rootfs
 	else
-		echo "Keine Settings auf SDA2 gefunden"
+		echo "Настройки в разделе SDA2 не найдены"
 		cd /
 		umount /dev/sda2
 		mount /dev/sda1 /rootfs
 	fi
 fi
-echo "Sichere Uboot"
+echo "Осторожненько запускаю загрузчик"
 cd /rootfs/boot
 cp uImage.gz /install
-echo "umount /dev/sda1"
+echo "Демонтирую загрузочный раздел /dev/sda1"
 cd /
 umount /dev/sda1
-echo "done"
-echo "FDISK" > /dev/vfd
-echo "Partitioniere Datenträger"
+echo "Готово"
+echo "Формат" > /dev/vfd
+echo "Готовлю структуру разбиения диска"
 HDD=/dev/sda
 ROOTFS=$HDD"1"
 SYSFS=$HDD"2"
@@ -74,7 +74,7 @@ DATAFS=$HDD"4"
 dd if=/dev/zero of=$HDD bs=512 count=64
 sfdisk --re-read $HDD
 # Löscht die Festplatte/Stick und erstellt 4 Partitionen
-#  1: 256MB Linux Uboot ext2
+#  1: 256MB Linux Uboot ext3
 #  2:   1GB Linux System ext4
 #  3: 256MB Swap
 #  4: rest freier Speicher LINUX ext4 (bei HDD record)
@@ -85,39 +85,39 @@ sfdisk $HDD -uM << EOF
 ,,L
 ;
 EOF
-echo "done"
-echo "Formatiere" > /dev/vfd
-echo "Formatiere Partitionen"
-echo "Format Uboot"
-mkfs.ext2 -I128 -b4096 -L BOOTFS $HDD"1"
-echo "Format System"
+echo "Готово"
+echo "Формат" > /dev/vfd
+echo "Начинаю форматирование..."
+echo "Форматирую загрузочный раздел"
+mkfs.ext3 -I 128 -b 4096 -L BOOTFS $HDD"1"
+echo "Форматирую системный раздел"
 mkfs.ext4 -L ROOTFS $HDD"2"
-echo "Formatiere Swap"
+echo "Форматирую раздел подкачки"
 mkswap $SWAPFS
-echo "Formatiere Rest Free Space"
+echo "Форматирую оставшееся свободное место"
 mkfs.ext4 -L RECORD $HDD"4"
-echo "done"
-echo "mounte /dev/sda2"
+echo "Готово"
+echo "Монтирую раздел /dev/sda2"
 mount /dev/sda2 /rootfs
-echo "Install" > /dev/vfd
-echo "Kopiere RootFS Image auf /dev/sda2..."
+echo "Загрузка" > /dev/vfd
+echo "Копирую системные файлы в системный раздел /dev/sda2..."
 cp /install/*.tar.gz /rootfs
 cd /rootfs
-echo "Lösche Image aus RAM ..."
+echo "Удаляю образ из Оперативной памяти..."
 rm /install/*.tar.gz
-echo "Installiere RootFS ..."
+echo "Устанавливаю системный раздел ..."
 tar -xf *.tar.gz
-echo "Lösche RootFS Image File"
+echo "Удаляю стартовый системный образ"
 rm /rootfs/*.tar.gz
-echo "done"
-echo "Restore Settings"
+echo "Готово"
+echo "Восстанавливаю настройки"
 if [ -e /install/backup/E2Settings.tar.gz ]; then
 	cp /install/backup/E2Settings.tar.gz /rootfs/etc/enigma2
 	cd /rootfs/etc/enigma2
 	tar -xf E2Settings.tar.gz
 	cd ../../..
 else
-	echo "keine Settings gesichert/restored"
+	echo "Нет данных для сохранения или восстановления"
 fi
 if [ -e /install/backup/keys.tar.gz ]; then
 	cp /install/backup/keys.tar.gz /rootfs/var/keys
@@ -125,15 +125,15 @@ if [ -e /install/backup/keys.tar.gz ]; then
 	tar -xf keys.tar.gz
 	cd ../../..
 else
-	echo "keine Keys gesichert/restored"
+	echo "Нет ключей для сохранения или восстановления"
 fi
 cd /
-echo "umount /dev/sda2 and mount /dev/sda1"
+echo "Демонтирую системный раздел /dev/sda2 и монтирую загрузочный раздел /dev/sda1"
 umount /dev/sda2
 sleep 1
 mount /dev/sda1 /rootfs
-echo "uImage" > /dev/vfd
-echo "Restore Uboot"
+echo "Загрузка" > /dev/vfd
+echo "Восстанавливаю загрузчик"
 mkdir /rootfs/boot
 cp /install/uImage.gz /rootfs/boot
 rm /install/uImage.gz
@@ -143,22 +143,21 @@ cd ../..
 sleep 2
 umount /dev/sda1
 sleep 1
-echo "FSCK" > /dev/vfd
-echo "Starte FSCK"
-fsck.ext2  -f -y /dev/sda1
+echo "Проверка" > /dev/vfd
+echo "Запускаю проверку дисков..."
+fsck.ext3  -f -y /dev/sda1
 sleep 1
 fsck.ext4  -f -y /dev/sda2
 sleep 1
 fsck.ext4  -f -y /dev/sda4
-echo "done"
+sleep 1
 echo "######################################################"
 echo ""
-echo "              Fertig, na dann auf gehts ...           "
+echo "   Всё готово!!! Сейчас начнётся загрузка сборки...   "
 echo ""
 echo "######################################################"
 sleep 1
 mount /dev/sda1 /rootfs
-echo "DONE..." > /dev/vfd
-echo "Starte Bootvorgang ..."
-
+echo "Готово" > /dev/vfd
+echo "Ну, с Богом!!! Поехали... ..."
 
