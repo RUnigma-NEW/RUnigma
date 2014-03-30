083dc26be4af5b57fc5fdad6dc4518af692b16c2
teslanet <teslanet07@gmail.com>
2014-03-30 16:56:31 +0300

    del e2

--- tdt/cvs/cdk/make.sh
+++ tdt/cvs/cdk/make.sh
@@ -257,8 +257,6 @@ EXTERNAL_LCD=""
 
 echo -e "\nГрафическая платформа:"
 echo "   1) Enigma2 OpenPli"
-#echo "   2) XBMC"
-#echo "   3) Enigma2"
 #case $8 in
 #        [1-3]) REPLY=$8
 #        echo -e "\nSelected Image: $REPLY\n"
@@ -271,15 +269,9 @@ echo "   1) Enigma2 OpenPli"
 #                if [ "$REPLY" == 1 ]; then
 #                    echo -e "\nВыберите один из вариантов:"
 #                        echo "   0) Самый свежий вариант   - E2 OpenPli gstreamer / libplayer3 (Возможны проблемы с компиляцией) "
-#                        echo "   1) Sat, 17 Mar 2012 19:51 - E2 OpenPli gstreamer              945aeb939308b3652b56bc6c577853369d54a537"
-#                        echo "   2) Sat, 18 Aug 2012 11:12 - E2 OpenPli gstreamer / libplayer3 4f3396b610f5524d85e06f51cbd3186b75f4b6d3"
-#                        echo "   3) Sat, 20 Aug 2012 19:08 - E2 OpenPli gstreamer / libplayer3 51a7b9349070830b5c75feddc52e97a1109e381e"
-#                        echo "   4) Sat, 24 Aug 2012 23:42 - E2 OpenPli gstreamer / libplayer3 002b85aa8350e9d8e88f75af48c3eb8a6cdfb880"
-#                        echo "   5) Fri, 05 Oct 2012 21:37 - E2 OpenPli gstreamer / libplayer3 7e38f7f6c911cd16106fb3b131e5c2d3a7ea51c7"
-#                        echo "   6) AR-P - E2 OpenPli branch testing"
-                        echo "   7) AR-P - E2 OpenPli branch last - по умолчанию"
-#                        echo "   8) AR-P - E2 OpenPli branch master"
-#                        echo "   9) Sat, 17 Sep 2012 17:19 - E2 OpenAAF gstreamer / libplayer3 0f7fa25f26091617213e85b0ed440beb67612ce3"
+                        echo "   1) AR-P - E2 OpenPli branch last - по умолчанию"
+#                        echo "   2) AR-P - E2 OpenPli branch master"
+#                        echo "   3) Sat, 17 Sep 2012 17:19 - E2 OpenAAF gstreamer / libplayer3 0f7fa25f26091617213e85b0ed440beb67612ce3"
 #                        echo ""
 #                        echo ""
 #                    read -p "Выберите enigma2 OpenPli (0-9):"
@@ -290,86 +282,13 @@ echo "   1) Enigma2 OpenPli"
 #                        2) IMAGE="--enable-e2pd2";;
 #                        3) IMAGE="--enable-e2pd3";;
 #                        4) IMAGE="--enable-e2pd4";;
-#                        5) IMAGE="--enable-e2pd5";;
-#                        6) IMAGE="--enable-e2pd6";;
-#                        7) IMAGE="--enable-e2pd7";;
-#                        8) IMAGE="--enable-e2pd8";;
-#                        9) IMAGE="--enable-e2pd9";;
-#                        *) IMAGE="--enable-e2pd7";;
-#                        esac
-#                elif [ "$REPLY" == 2 ]; then
-#                    echo -e "\nВыберите один из вариантов:"
-#                        echo "  0) Самый свежий вариант (Возможны проблемы с компиляцией)"
-#                        echo "  1) Sat, 14 Apr 2012 12:36 - 460e79416c5cb13010456794f36f89d49d25da75"
-#                        echo "  2) Sun, 10 Jun 2012 13:53 - 327710767d2257dad27e3885effba1d49d4557f0"
-#                        echo "  3) Fr,  31 Aug 2012 22:34 - Frodo_alpha5 - 12840c28d8fbfd71c26be798ff6b13828b05b168"
-#                        echo "  4) Fr,  31 Oct 2012 22:34 - Frodo_alpha7 - e292b1147bd89a7e53742e3e5039b9a906a3b1d0"
-#                        echo ""
-#                        echo ""
-#                    read -p "Выберите XBMC (0-4):"
-#                        case "$REPLY" in
-#                        0) IMAGE="--enable-xbd0" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        1) IMAGE="--enable-xbd1" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        2) IMAGE="--enable-xbd2" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        3) IMAGE="--enable-xbd3" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        4) IMAGE="--enable-xbd4" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        *) IMAGE="--enable-xbd3" GFW="--enable-graphicfwdirectfb" MEDIAFW="--enable-mediafwgstreamer";;
-#                        esac
-#                elif [ "$REPLY" == 3 ]; then
-#                    echo -e "\nВыберите один из вариантов:"
-#                        echo "  0) Самый свежий вариант (Возможны проблемы с компиляцией)"
-#                        echo "  1) Sat, 29 Mar 2011 13:49 - E2 V3.0 e013d09af0e010f15e225a12dcc217abc052ee19"
-#                        echo "  2) inactive"
-#                        echo "  3) inactive"
-#                        echo "  4) inactive"
-#                        echo "  5) Fri,  5 Nov 2010 00:16 - E2 V2.4 libplayer3 7fd4241a1d7b8d7c36385860b24882636517473b"
-#                        echo "  6) Wed,  6 Jul 2011 11:17 - E2 V3.1 gstreamer  388dcd814d4e99720cb9a6c769611be4951e4ad4"
-#                        echo "  7) Current E2 gitgui arp-team gstreamer / eplayer3 - по умолчанию"
-#                        echo "  8) Current E2 gitgui arp-team no gstreamer"
-#                        echo ""
-                        echo ""
-#                    read -p "Выберите enigma2 (0-8):"
-#                        case "$REPLY" in
-#                        0) IMAGE="--enable-e2d0";;
-#                        1) IMAGE="--enable-e2d1";;
-#                        2) IMAGE="--enable-e2d2";;
-#                        3) IMAGE="--enable-e2d3";;
-#                        4) IMAGE="--enable-e2d4";;
-#                        5) IMAGE="--enable-e2d5";;
-#                        6) IMAGE="--enable-e2d6";;
-#                        7) IMAGE="--enable-e2d7";;
-#                        8) IMAGE="--enable-e2d8";;
-#                        *) IMAGE="--enable-e2d7";;
-#                        esac
-#                elif  [ "$REPLY" == 4 ]; then
-#                    echo -e "\nChoose VDR revisions"
-#                        echo "   1) VDR-1.7.22"
-#                        echo "   2) VDR-1.7.27"
-#                    read -p "Select VDR-1.7.XX (1-2)? "
-#                        case "$REPLY" in
-#                        1) IMAGE="--enable-vdr1722"
-#                            cd ../apps/vdr/
-#                        if [ -L vdr ]; then
-#                            rm vdr
-#                        fi
-#                            ln -s vdr-1.7.22 vdr
-#                        cd -
-#                        ;;
-#                        2) IMAGE="--enable-vdr1727"
-#                            cd ../apps/vdr/
-#                        if [ -L vdr ]; then
-#                            rm vdr
-#                        fi
-#                            ln -s vdr-1.7.27 vdr
-#                        cd -
-#                        ;;
-#                        *) IMAGE="--enable-vdr1722";;
+#                        *) IMAGE="--enable-e2pd1";;
 #                        esac
 #                fi
 #                ;;
 #        *)
 #esac
-IMAGE="--enable-e2pd7"
+IMAGE="--enable-e2pd1"
 ##############################################
 case "$IMAGE" in
     --enable-e2*)
