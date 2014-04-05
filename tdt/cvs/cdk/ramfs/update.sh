0bd825fbf10a5720b36eb4f465a022901a134650
teslanet <teslanet07@gmail.com>
2014-04-05 22:55:13 +0300

    fix update.sh

--- tdt/cvs/cdk/ramfs/update.sh
+++ tdt/cvs/cdk/ramfs/update.sh
@@ -1,12 +1,9 @@
 echo "Обновляю" > /dev/vfd
-# Kopiert das Update File auf die /dev/sda1 Partition um es Später zu installieren
-rm /rootfs/service/update
-# Entpackt das Update und löscht es anschliessend
 cd /rootfs
 tar -xf service/*.tar.gz
 echo "Готово"
 echo "Удаляю tar.gz из системного раздела"
-rm /rootfs/service/*.tar.gz
+rm -rf /rootfs/service
 echo "Обновление завершено..."
 cd /
 echo "Запускаю систему..."
